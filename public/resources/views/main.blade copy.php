beeb9901e38df7376f9e9cf9dfe5282ea49d79d4
HyunjinHa <hahj123456@gmail.com>
2023-12-14 00:49:51 +0900

    css 및 메인 페이지 추가

---
+++ public/resources/views/main.blade copy.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>petShop home</title>
+    <meta name="keywords" content=""><meta name="robots" content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1">
+    <meta name="description" content="">
+    <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900%7CQuicksand:400,700%7CQuestrial" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" media="all" href="{{ asset('css/default.css') }}">
+    <link rel="stylesheet" type="text/css" media="all" href="{{ asset('css/fonts.css') }}">
+    <!--[if IE 6]><link href="default_ie6.css" rel="stylesheet" type="text/css" /><![endif]-->
+  </head>
+  <body>
+    <div id="header-wrapper">
+      <div id="header" class="container">
+        <div id="logo">
+          <h1><a href="#">petShop</a></h1>
+        </div>
+        <div id="menu">
+          <ul><li class="current_page_item"><a href="#" accesskey="1" title="">Homepage</a></li>
+            <li><a href="#" accesskey="2" title="">Gallery</a></li>
+            <li><a href="#" accesskey="3" title="">Mypage</a></li>
+          </ul></div>
+      </div>
+      <div id="banner" class="container"><img src="images/banner.jpg" width="1200" height="400" alt=""></div>
+    </div>
+    <div class="copyright">
+      Design by: <a href="https://templated.co/">TEMPLATED.CO</a>
+    </div>
+  </body>
+</html>
---
+++ public/resources/views/main.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>petShop home</title>
+    <meta name="keywords" content="">
+    <meta name="robots" content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1">
+    <meta name="description" content="">
+    <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900%7CQuicksand:400,700%7CQuestrial" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" media="all" href="{{ asset('css/default.css') }}">
+    <link rel="stylesheet" type="text/css" media="all" href="{{ asset('css/fonts.css') }}">
+    <!--[if IE 6]><link href="default_ie6.css" rel="stylesheet" type="text/css" /><![endif]-->
+</head>
+<body>
+    <div id="header-wrapper">
+        <div id="header" class="container">
+            <div id="logo">
+                <h1><a href="#">petShop</a></h1>
+            </div>
+            <div id="menu">
+                <ul>
+                    <li class="current_page_item"><a href="#" accesskey="1" title="homepage">Homepage</a></li>
+                    <li><a href="#" accesskey="2" title="galley">Gallery</a></li>
+                    <li><a href="#" accesskey="3" title="mypage">Mypage</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="copyright">
+        Design by: <a href="https://templated.co/">TEMPLATED.CO</a>
+    </div>
+</body>
+</html>
